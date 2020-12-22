b5baf5446fa8c8952917c19c04aabcc9cdd08be2
VuongTungLan <tt.lan.vuong@tomosia.com>
2020-12-22 15:45:51 +0700

    change 7

--- script.js
+++ script.js
@@ -24,15 +24,15 @@ video.addEventListener('play', () => {
     const detection = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detection, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    if (resizedDetections.length > 0) {
-      const box = resizedDetections.detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: 'Lan' })
-      // faceapi.draw.drawDetections(canvas, resizedDetections)
-      // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-      // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
-      drawBox.draw(canvas)
-    }
+    const box = resizedDetections.detection.box
+    const drawBox = new faceapi.draw.DrawBox(box, { label: 'Lan' })
+    // faceapi.draw.drawDetections(canvas, resizedDetections)
+    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+    drawBox.draw(canvas)
+
 
   }, 100)
 })
